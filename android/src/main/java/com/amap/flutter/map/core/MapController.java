1b1885875d19afef8a3ac8ba2eb09846459c3663
longch <cl_knight51@163.com>
2021-10-28 15:43:48 +0800

    hidden amap text

--- android/src/main/java/com/amap/flutter/map/core/MapController.java
+++ android/src/main/java/com/amap/flutter/map/core/MapController.java
@@ -168,6 +168,8 @@ public class MapController
                 mapReadyResult.success(null);
                 mapReadyResult = null;
             }
+                
+            amap.showMapText(false);
         } catch (Throwable e) {
             LogUtil.e(CLASS_NAME, "onMapLoaded", e);
         }
